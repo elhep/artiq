0ac6ee0712967e7ee5b97ffb055327c3fac885a8
Mikolaj Sowinski <mikolaj.sowinski@gmail.com>
2019-12-03 00:24:58 +0100

    Added FMC extension type

---
+++ artiq/gateware/fmc.py
@@ -0,0 +1,31 @@
+def _fmc_pin(fmc: str, bank: str, i: int, pol: str):
+    bank = bank.upper()
+    pol = pol.upper()
+    cc_pin_name_tmp = "fmc{}:{}{:02d}_CC_{}"
+    pin_name_tmp = "fmc{}:{}{:02d}_{}"
+    cc_pins = {
+        "LA": [0, 1, 17, 18],
+        "HA": [0, 1, 17],
+        "HB": [0, 6, 17],
+    }
+    if i in cc_pins[bank]:
+        return cc_pin_name_tmp.format(fmc, bank, i, pol)
+    else:
+        return pin_name_tmp.format(bank, i, pol)
+
+
+class _FMC:
+    @classmethod
+    def add_extension(cls, target, fmc, iostd_single, iostd_diff):
+        name = cls.__name__
+        target.platform.add_extension(cls.io(fmc, iostd_single, iostd_diff))
+        print("{} (FMC{}) starting at RTIO channel 0x{:06x}"
+              .format(name, fmc, len(target.rtio_channels)))
+
+    @classmethod
+    def io(cls, fmc, iostd_single, iostd_diff):
+        raise NotImplementedError
+
+
+
+
